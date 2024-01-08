68b2a01e48636c1c62ed3de7d03f9cf9ef723f31
Nicolas Graves <ngraves@ngraves.fr>
2024-01-08 09:02:45 +0100

    build-system: copy: Redefine copy-build.
    * guix/build-system/copy.scm
    (copy-build): Monadic procedure returns a gexp instead of a derivation.
    
    Change-Id: I96dfa099501796df007143db63a49e2adedbee92

--- guix/build-system/copy.scm
+++ guix/build-system/copy.scm
@@ -3,6 +3,7 @@
 ;;; Copyright © 2020 Pierre Neidhardt <mail@ambrevar.xyz>
 ;;; Copyright © 2021, 2022 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2023 Jonathan Brielmaier <jonathan.brielmaier@web.de>
+;;; Copyright © 2024 Nicolas Graves <ngraves@ngraves.fr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -118,14 +119,8 @@
                             #:strip-flags #$strip-flags
                             #:strip-directories #$strip-directories)))))
 
-  (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
-                                                  system #:graft? #f)))
-    (gexp->derivation name builder
-                      #:system system
-                      #:target #f
-                      #:substitutable? substitutable?
-                      #:graft? #f
-                      #:guile-for-build guile)))
+  (mbegin %store-monad
+    (return builder)))
 
 (define copy-build-system
   (build-system
