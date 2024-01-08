b73f378b1840cc9e5b82addfdb9f01d407d4a58d
Nicolas Graves <ngraves@ngraves.fr>
2024-01-08 09:02:42 +0100

    build-system: clojure: Redefine clojure-build.
    * guix/build-system/clojure.scm
    (clojure-build): Monadic procedure returns a gexp instead of a derivation.
    
    Change-Id: I5b5b552052cfffc45bc4d82871600b322eb23d85

--- guix/build-system/clojure.scm
+++ guix/build-system/clojure.scm
@@ -1,6 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2018 Alex Vong <alexvong1995@gmail.com>
 ;;; Copyright © 2020, 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2024 Nicolas Graves <ngraves@ngraves.fr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -169,11 +170,8 @@
                          #:system #$system
                          #:inputs #$(input-tuples->gexp inputs)))))
 
-  (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
-                                                  system #:graft? #f)))
-    (gexp->derivation name builder
-                      #:system system
-                      #:guile-for-build guile)))
+  (mbegin %store-monad
+    (return builder)))
 
 (define clojure-build-system
   (build-system
