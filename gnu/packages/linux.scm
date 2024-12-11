abe4640b995a6852addee1f483caeac72d53c731
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-12-11 18:07:05 +0100

    gnu: Use cryptsetup-minimal instead of cryptsetup.
    * gnu/packages/admin.scm (pam-mount)[inputs]: Use cryptsetup-minimal instead
    of cryptsetup.
    * gnu/packages/crypto.scm (tomb)[inputs]: Likewise.
    * gnu/packages/linux.scm (ecryptfs-utils)[inputs]: Likewise.
    * gnu/packages/password-utils.scm (bruteforce-luks)[inputs]: Likewise.
    * gnu/packages/storage.scm (ceph)[inputs]: Likewise.
    * gnu/packages/virtualization.scm (ganeti-instance-guix)[inputs]: Likewise.
    * gnu/packages/disk.scm (volume-key)[inputs]: Likewise.
    (libblockdev)[inputs]: Likewise.
    * gnu/packages/freedesktop.scm (udisks)[inputs]: Likewise.
    * gnu/system/install.scm (%installer-disk-utilities)[inputs]: Likewise.
    
    Change-Id: Ib48ae1b45f6c63edd620a932241f2aa57567a350

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -8027,7 +8027,7 @@ graphically visualizing a @file{perf.data} file.")
            pkg-config))
     (inputs
      (list coreutils
-           cryptsetup
+           cryptsetup-minimal
            findutils
            gawk
            grep
