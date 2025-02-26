d996e09965d860dfe538e71bc381043f472ff4b2
Morgan Smith <Morgan.J.Smith@outlook.com>
2025-02-26 12:39:46 -0500

    teams: Register the comp-integrity.el file to the Emacs team.
    * etc/teams.scm (emacs)[#:scope]: Add
    “gnu/packages/aux-files/emacs/comp-integrity.el”.
    
    Change-Id: I66014b94e73fd87eeb3aceaf5f61f08abc875c44
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- etc/teams.scm
+++ etc/teams.scm
@@ -268,6 +268,7 @@ exec $pre_inst_env_maybe guix repl -- "$0" "$@"
         #:description "The extensible, customizable text editor and its
 ecosystem."
         #:scope (list "gnu/packages/aux-files/emacs/guix-emacs.el"
+                      "gnu/packages/aux-files/emacs/comp-integrity.el"
                       (make-regexp* "^gnu/packages/emacs(-.+|)\\.scm$")
                       "gnu/packages/tree-sitter.scm"
                       "guix/build/emacs-build-system.scm"
