176cba230f2eb98b3414b8c8ebade02e0cf51145
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-02 15:15:24 +0100

    gnu: emacs-org-cliplink: Use implicit “ert-runner”.
    * gnu/packages/emacs-xyz.scm (emacs-org-cliplink)[arguments]:
    Drop #:test-command.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -42983,7 +42983,6 @@ should pop up.")
       (build-system emacs-build-system)
       (arguments
        (list
-        #:test-command #~(list "ert-runner")
         #:phases
         #~(modify-phases %standard-phases
             (add-after 'unpack 'patch-curl-executable
