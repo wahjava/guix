31a9086d885bd76b85161080a4b1d563e81e137d
Morgan Smith <Morgan.J.Smith@outlook.com>
2025-03-02 17:15:14 -0500

    gnu: emacs-treemacs-extra: Use '#:lisp-directory'.
    * gnu/packages/emacs-xyz.scm (emacs-treemacs-extra): Use
    '#:lisp-directory'.
    
    Change-Id: I1e03a7d2feb03e6f0a5634c9dee06d45d152391d
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -34332,7 +34332,7 @@ utilities.")
          (package-arguments emacs-treemacs)
        ((#:phases phases)
         #~(modify-phases #$phases
-            (add-after 'chdir-elisp 'copy-extra
+            (add-after 'unpack 'copy-extra
               (lambda _
                 (copy-recursively "../extra" ".")))))))
     (propagated-inputs
