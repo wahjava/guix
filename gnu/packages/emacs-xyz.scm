da0e578a4150140d017f7ea05baa9d239fa6924d
Nicolas Graves <ngraves@ngraves.fr>
2025-03-20 11:08:27 +0100

    gnu: emacs-spamfilter-el: Disable tests.
    * gnu/packages/emacs-xyz.scm (emacs-spamfilter-el)
    [arguments]{tests?}: Disable them.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -41596,6 +41596,7 @@ comfortable presentation.")
     (build-system emacs-build-system)
     (arguments
      (list
+      #:tests? #f  ; No tests.
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'convert-encoding
