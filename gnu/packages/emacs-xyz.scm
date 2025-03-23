276f95e9c3567e2e95992e26503711a41aeeb990
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-23 13:01:24 +0100

    gnu: emacs-lin: Skip tests.
    * gnu/packages/emacs-xyz.scm (emacs-lin)[arguments]: Add #:tests? #f.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -20920,6 +20920,7 @@ number on the left margin in Emacs.")
     (build-system emacs-build-system)
     (arguments
      (list
+      #:tests? #f                       ; no tests
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'install 'makeinfo
