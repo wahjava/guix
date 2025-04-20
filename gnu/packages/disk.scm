01f9957f0403a48d38c6202bd14a2bf297edbb23
Grigory Shepelev <shegeley@gmail.com>
2025-04-20 17:14:50 +0300

    gnu: disks: add open-isns
    Change-Id: I63f14426888d0482a1aee1d1c1a60c00c8a4e75d

--- gnu/packages/disk.scm
+++ gnu/packages/disk.scm
@@ -108,6 +108,7 @@
   #:use-module (gnu packages web)
   #:use-module (gnu packages xml)
   #:use-module (gnu packages xorg)
+  #:use-module (gnu packages ninja)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system glib-or-gtk)
   #:use-module (guix build-system go)
@@ -683,6 +684,50 @@ transport), SCSI and ATAPI tape drives, and SCSI enclosures.  This utility can
 also send commands associated with starting and stopping the media, loading
 and unloading removable media and some other housekeeping functions.")
     (license license:bsd-3)))
+
+(define-public open-isns
+  (package
+    (name "open-isns")
+    (version "0.103")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/open-iscsi/open-isns")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (modules '((guix build utils)))
+       (sha256
+        (base32 "0008f4sv0z3mmqgf5ifx6rp7nwpp41qd3shhi3b2ci1ir8r91skf"))))
+    (build-system meson-build-system)
+    (arguments
+     (list
+      #:tests? #f
+      ;; couldn't figure out proper testing in ninja. I am not an expert
+      ;; tried:
+      ;; (replace 'check (lambda _ (invoke "ninja" "-C" (getcwd) "test")))
+      ;; didn't worked
+      ;; fails with FileNotFoundError: [Errno 2] No such file or directory: '/tmp/guix-build-open-isns-0.103.drv-0/source/tests/test-isns.py'
+      #:configure-flags
+      #~(list "--sharedstatedir=/var" "-Dslp=disabled"
+              (string-append "-Dsystemddir="
+                             #$output "/lib/systemd"))
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'fix-meson.build
+            (lambda* (#:key outputs #:allow-other-keys)
+              (substitute* "meson.build"
+                (("/usr/lib64")
+                 (string-append (assoc-ref outputs "out") "/lib"))
+                (("/var/lib")
+                 (string-append (assoc-ref outputs "out") "/lib"))))))))
+    (native-inputs (list meson pkg-config ninja))
+    (inputs (list openssl))
+    (home-page "https://github.com/open-iscsi/open-isns")
+    (synopsis "iSNS server and client for Linux")
+    (description
+     "Partial implementation of @acronym{iSNS, Internet Storage Name Service}, according to @url{https://www.rfc-editor.org/rfc/rfc3720.html, RFC4171"})
+    (license license:gpl2)))
 
 (define-public idle3-tools
   (package
