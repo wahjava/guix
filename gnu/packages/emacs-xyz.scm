2a7e23852a26050c53040d4df1346b84663f0762
Nicolas Graves <ngraves@ngraves.fr>
2025-04-27 16:52:41 +0200

    gnu: emacs-org-board: Skip tests.
    * gnu/packages/emacs-xyz.scm (emacs-org-board): Skip tests.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -31240,6 +31240,7 @@ incompatible versions of it.")
       (build-system emacs-build-system)
       (arguments
        (list
+        #:tests? #f  ; No tests.
         #:phases
         #~(modify-phases %standard-phases
             (add-after 'unpack 'set-wget-location
