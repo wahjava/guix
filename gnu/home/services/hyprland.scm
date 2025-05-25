bd509b11c84b72c9d444687f1e1c802749d15154
Carmine Margiotta <email@cmargiotta.net>
2025-05-25 23:53:48 +0200

    home: services: fix typo Hyprland

--- gnu/home/services/hyprland.scm
+++ gnu/home/services/hyprland.scm
@@ -16,7 +16,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (home home services hyprland)
+(define-module (gnu home services hyprland)
   #:use-module (gnu packages wm)
   #:use-module (guix gexp)
   #:use-module (guix modules)
