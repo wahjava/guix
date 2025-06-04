4a290be39833a65ba4620b66a2137020fcbc81ef
Romain GARBAGE <romain.garbage@inria.fr>
2025-06-04 14:59:21 +0200

    services: Import cloud-init service from rde.
    * gnu/services/openstack.scm: New file.
    * gnu/local.mk (GNU_SYSTEM_MODULES): Add file.
    
    Change-Id: Ia7e495ff1228fd825c38dce3fc0a40b7c438aa6a

--- gnu/local.mk
+++ gnu/local.mk
@@ -764,6 +764,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/services/lightdm.scm      		\
   %D%/services/linux.scm			\
   %D%/services/lirc.scm				\
+  %D%/services/openstack.scm			\
   %D%/services/virtualization.scm		\
   %D%/services/mail.scm				\
   %D%/services/mcron.scm			\
