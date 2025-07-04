12c2c0d183b0e9429fa1294fdf6a302fb5f9cedd
Cayetano Santos <csantosb@inventati.org>
2025-07-04 16:54:32 +0200

    gnu: emacs-corfu: Fix indenting and max column.
    * gnu/packages/emacs-xyz.scm (emacs-corfu): Fix indenting and max column.
    
    Change-Id: If01f89bd7eaaca21e3a0d9a89f0c38970e69947e

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -5916,8 +5916,8 @@ of bibliographic references.")
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/minad/corfu")
-             (commit version)))
+              (url "https://github.com/minad/corfu")
+              (commit version)))
        (file-name (git-file-name name version))
        (sha256
         (base32 "1f2hz1970f5ifd5p6lbxdi4krgxgvxrr477f6vbqakqj07bk9jps"))))
@@ -5927,8 +5927,8 @@ of bibliographic references.")
       #:tests? #f ; there are no tests
       #:phases
       #~(modify-phases %standard-phases
-          ;; Move the extensions source files to the top level, which is included
-          ;; in the EMACSLOADPATH.
+          ;; Move the extensions source files to the top level, which is
+          ;; included in the EMACSLOADPATH.
           (add-after 'unpack 'move-source-files
             (lambda _
               (let ((el-files (find-files "./extensions" ".*\\.el$")))
