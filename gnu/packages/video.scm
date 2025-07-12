5c425055f736b748f8771fadfa9303f9cdfc8481
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-13 00:40:04 +0900

    gnu: ffmpeg-7: Enable libplacebo support.
    * gnu/packages/video.scm (ffmpeg-7) [inputs]: Add libplacebo.
    [configure-flags]: Add --enable-libplacebo.
    
    Change-Id: Ib46d46505417b63d44d28369662617e8ebbf9098

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1668,6 +1668,7 @@ operate properly.")
             libcaca
             libcdio-paranoia
             libdrm
+            libplacebo
             libtheora
             libva
             libvdpau
@@ -1769,6 +1770,7 @@ operate properly.")
          "--enable-libfreetype"
          "--enable-libmp3lame"
          "--enable-libopus"
+         "--enable-libplacebo"
          "--enable-libpulse"
          #$@(if (this-package-input "rav1e")
                 '("--enable-librav1e")
