3edef38104e6219d8fcb02a888a4fca173ed5815
nomike <nomike@nomike.com>
2025-07-13 04:28:19 +0200

    gnu: prusa-slicer: Update to 2.9.2
    * gnu/packages/engineering.scm (prusa-slicer): Update to 2.9.2
    [source]: Remove patch "prusa-slicer-fix-tests.patch" as it is obsolete
    and fix detection of hidapi in the CMake scripts.
    [native-inputs]: Update catch2 to version 3.8 as required by the project.
    [inputs]: Downgrade opencascade-occt to version 7.6.1 as mandated by the project
    and add webkitgtk-for-gtk3 and webkitgtk-with-libsoup2.
    [description]: Extend description based on the project's original one.
    
    Change-Id: I9074e4849464df01e6bad84de9149c3ce9e2cf65

--- gnu/packages/engineering.scm
+++ gnu/packages/engineering.scm
@@ -179,6 +179,7 @@
   #:use-module (gnu packages tree-sitter)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages web)
+  #:use-module (gnu packages webkit)
   #:use-module (gnu packages wxwidgets)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages xiph)
@@ -4594,102 +4595,128 @@ G-codes to binary and vice versa.")
 (define-public prusa-slicer
   (package
     (name "prusa-slicer")
-    (version "2.7.4")
+    (version "2.9.2")
     (source
      (origin
        (method git-fetch)
-       (uri
-        (git-reference
-         (url "https://github.com/prusa3d/PrusaSlicer")
-         (commit (string-append "version_" version))))
+       (uri (git-reference
+             (url "https://github.com/prusa3d/PrusaSlicer")
+             (commit (string-append "version_" version))))
        (file-name (git-file-name name version))
-       (sha256 (base32 "0s1cfvhfilyv0y98asr61c6rwlgyr1hf5v5hg8q9zwmzm2bkcql3"))
-       (patches (search-patches "prusa-slicer-fix-tests.patch"))
+       (patches (search-patches
+                 "prusa-slicer-2.9.2-Add-CMake-module-for-finding-HidAPI-library.patch"))
+       (sha256
+        (base32 "05zwwhqv3fjg9rx6a4ga55f4ic1136f6lwms0kb4kaq50w9dvxwg"))
        (modules '((guix build utils)))
-       (snippet
-        `(begin
-           ;; Prusa slicer bundles a lot of dependencies in src/ directory.
-           ;; Most of them contain prusa-specific modifications (e.g. avrdude),
-           ;; but others do not. Here we replace the latter with Guix packages.
-           ;; Remove bundled libraries that were not modified by Prusa Slicer developers.
-           (delete-file-recursively "src/hidapi")
-           (delete-file-recursively "src/eigen")
-           (delete-file-recursively "src/libigl/igl")
-           (substitute* "CMakeLists.txt"
-             (("add_library\\(libexpat INTERFACE\\)")
-              ""))
-           (substitute* "src/libigl/CMakeLists.txt"
-             (("target_link_libraries\\(libigl INTERFACE igl::core\\)") ""))
-           (substitute* "src/CMakeLists.txt"
-             (("add_subdirectory\\(hidapi\\)")
-              "pkg_check_modules(HIDAPI REQUIRED hidapi-hidraw)")
-             (("include_directories\\(hidapi/include\\)")
-              "include_directories()"))
-           (substitute* "src/slic3r/CMakeLists.txt"
-             (("add_library\\(libslic3r_gui.*" all)
-              (string-append
-               all
-               "\ntarget_include_directories(libslic3r_gui PUBLIC ${HIDAPI_INCLUDE_DIRS})\n"))
-             (("\\bhidapi\\b") "${HIDAPI_LIBRARIES}"))))))
+       (snippet `(begin
+                   ;; Prusa slicer bundles a lot of dependencies in src/ directory.
+                   ;; Most of them contain prusa-specific modifications (e.g. avrdude),
+                   ;; but others do not. Here we replace the latter with Guix packages.
+                   ;; Remove bundled libraries that were not modified by Prusa Slicer
+                   ;; developers.
+                   (delete-file-recursively "bundled_deps/hidapi")
+                   ;; (delete-file-recursively "bundled_deps/eigen")
+                   (delete-file-recursively "bundled_deps/libigl/igl")
+                   (substitute* "CMakeLists.txt"
+                     (("add_library\\(libexpat INTERFACE\\)")
+                      "")
+                     (("target_link_libraries\\(libexpat INTERFACE EXPAT::EXPAT\\)")
+                      ""))
+                   (substitute* "bundled_deps/CMakeLists.txt"
+                     (("add_subdirectory\\(hidapi\\)")
+                      ""))
+                   (substitute* "bundled_deps/libigl/CMakeLists.txt"
+                     (("target_link_libraries\\(libigl INTERFACE igl::core\\)")
+                      ""))
+                   (substitute* "src/CMakeLists.txt"
+                     (("add_library\\(libexpat INTERFACE\\)")
+                      "")
+                     (("target_link_libraries\\(libexpat INTERFACE EXPAT::EXPAT\\)")
+                      "")
+                     (("add_subdirectory\\(hidapi\\)")
+                      "pkg_check_modules(HIDAPI REQUIRED hidapi-hidraw)")
+                     (("include_directories\\(hidapi/include\\)")
+                      "include_directories()")
+                     (("list\\(APPEND wxWidgets_LIBRARIES libexpat\\)")
+                      "list(APPEND wxWidgets_LIBRARIES expat)"))
+                   (substitute* "src/libslic3r/CMakeLists.txt"
+                     (("libexpat")
+                      "expat"))
+                   (substitute* "src/slic3r/CMakeLists.txt"
+                     (("add_library\\(libslic3r_gui.*" all)
+                      (string-append "find_package(HidAPI REQUIRED)\n" all
+                       "
+target_include_directories(libslic3r_gui PRIVATE ${HIDAPI_INCLUDE_DIR})
+"))
+                     (("    hidapi")
+                      "    ${HIDAPI_LIBRARY}"))))))
     (build-system cmake-build-system)
     (arguments
-     (list #:configure-flags
-           #~(list "-DSLIC3R_FHS=1" ;; Use The Filesystem Hierarchy Standard.
-                   "-DSLIC3R_GTK=3" ;; Use GTK+
-                   ;; Use wxWidgets 3.0.x.x to prevent GUI crashes when adding support enforcers.
-                   "-DSLIC3R_WX_STABLE=1"
-                   (format #f "-Dlibigl_DIR=~a"
-                           (search-input-directory %build-inputs
-                                                   "lib/cmake/igl/"))
-                   (format #f "-DCatch2_DIR=~a"
-                           (search-input-directory %build-inputs
-                                                   "lib/cmake/Catch2/")))
-           #:phases
-           #~(modify-phases %standard-phases
-               (add-after 'unpack 'fix-include-paths
-                 (lambda _
-                   (substitute* "tests/libslic3r/test_quadric_edge_collapse.cpp"
-                     (("#include <libigl/igl/qslim.h>")
-                      "#include <igl/qslim.h>")))))))
-    (native-inputs
-     (list pkg-config catch2))
-    (inputs
-     (list boost
-           cereal
-           cgal
-           curl
-           dbus
-           eigen
-           eudev
-           expat
-           glew
-           glib
-           gmp
-           gtk+
-           heatshrink
-           hidapi
-           ilmbase
-           libigl
-           libjpeg-turbo
-           libpng
-           mesa
-           mpfr
-           nanosvg
-           nlopt
-           opencascade-occt
-           openvdb
-           pango
-           prusa-libbgcode
-           ;; XXX: Using Prusa wxWidgets fork as PrusaSlicer segfaults when compiled
-           ;; with regular wxwidgets.
-           prusa-wxwidgets
-           qhull
-           tbb
-           zlib))
+     (list
+      #:configure-flags
+      #~(list "-DSLIC3R_FHS=1" ;Use The Filesystem Hierarchy Standard.
+              "-DSLIC3R_GTK=3" ;Use GTK+
+              ;; Use wxWidgets 3.0.x.x to prevent GUI crashes when adding
+              ;; support enforcers.
+              "-DSLIC3R_WX_STABLE=1"
+              (format #f "-Dlibigl_DIR=~a"
+                      (search-input-directory %build-inputs "lib/cmake/igl/"))
+              (format #f "-DCatch2_DIR=~a"
+                      (search-input-directory %build-inputs
+                                              "lib/cmake/Catch2/")))
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'fix-include-paths
+            (lambda _
+              (substitute* "tests/libslic3r/test_quadric_edge_collapse.cpp"
+                (("#include <libigl/igl/qslim.h>")
+                 "#include <igl/qslim.h>")))))))
+    (native-inputs (list pkg-config catch2-3.8))
+    (inputs (list boost
+                  cereal
+                  cgal
+                  curl
+                  dbus
+                  eigen
+                  eudev
+                  expat
+                  glew
+                  glib
+                  gmp
+                  gtk+
+                  heatshrink
+                  hidapi
+                  ilmbase
+                  libigl
+                  libjpeg-turbo
+                  libpng
+                  mesa
+                  mpfr
+                  nanosvg
+                  nlopt
+                  opencascade-occt-7.6.1
+                  openssl
+                  openvdb
+                  pango
+                  prusa-libbgcode
+                  ;; XXX: Using Prusa wxWidgets fork as PrusaSlicer segfaults
+                  ;; when compiled with regular wxwidgets.
+                  prusa-wxwidgets
+                  qhull
+                  tbb
+                  webkitgtk-for-gtk3
+                  webkitgtk-with-libsoup2
+                  z3
+                  zlib))
     (home-page "https://www.prusa3d.com/prusaslicer/")
-    (synopsis "G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc.)")
-    (description "PrusaSlicer takes 3D models (STL, OBJ, AMF) and converts them into
-G-code instructions for FFF printers or PNG layers for mSLA 3D printers.")
+    (synopsis
+     "G-code generator for 3D printers (RepRap, Makerbot, Ultimaker etc.)")
+    (description
+     "PrusaSlicer takes 3D models (STL, OBJ, AMF) and converts them into
+G-code instructions for FFF printers or PNG layers for mSLA 3D printers.  It's
+compatible with any modern printer based on the RepRap toolchain, including
+all those based on the Marlin, Prusa, Sprinter and Repetier firmware.  It also
+works with Mach3, LinuxCNC and Machinekit controllers.")
     (license license:agpl3)
 
     ;; Mark as tunable to take advantage of SIMD code in Eigen and in libigl.
