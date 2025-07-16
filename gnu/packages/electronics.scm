76ef9b9785aa869a0e4529c2d8457c0104f8e19d
Cayetano Santos <csantosb@inventati.org>
2025-07-16 11:41:38 +0200

    gnu: python-vsg: Update to 3.33.0.
    * gnu/packages/electronics.scm (python-vsg): Update to 3.33.0.
    [source]: Fix indenting.
    [arguments]<:#test-flags>: Disable test_utf_8.
    [native-nputs]: Add pytest-cov and pytest-html.
    
    Change-Id: I9ed0c4df95140e54301995c268dc6f9206d44c53

--- gnu/packages/electronics.scm
+++ gnu/packages/electronics.scm
@@ -767,23 +767,27 @@ design.")
 (define-public python-vsg
   (package
     (name "python-vsg")
-    (version "3.32.0")
+    (version "3.33.0")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/jeremiah-c-leary/vhdl-style-guide/")
-             (commit version)))
+              (url "https://github.com/jeremiah-c-leary/vhdl-style-guide/")
+              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0ql96n291zm4j324q8fmlvy8xvrksb8v6fip0g0sw374z86hda53"))))
+        (base32 "1pnhha7dfika5jv1wrdwjkwrqaz22n0fb845wid5sy62gn549hmb"))))
     (build-system pyproject-build-system)
     (arguments
      (list
       #:test-flags
       ;; Tests are expensive and may introduce race condition on systems with
       ;; high (more than 16) threads count; limit parallel jobs to 8x.
-      #~(list "--numprocesses" (number->string (min 8 (parallel-job-count))))
+      #~(list "--numprocesses" (number->string (min 8 (parallel-job-count)))
+              ;; TODO: Remove in 3.34.0.
+              ;; "file" command on "utf-8_encoded.vhd" file fails to detect
+              ;; utf-8 formatting. See #1471 upstream.
+              "-vv" "-k" "not test_utf_8")
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'pathch-pytest-options
@@ -794,6 +798,8 @@ design.")
                 ((".*-n.*auto.*") "")))))))
     (native-inputs
      (list python-pytest
+           python-pytest-cov
+           python-pytest-html
            python-pytest-xdist
            python-setuptools
            python-wheel))
