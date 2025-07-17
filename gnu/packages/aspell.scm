551b9ff034b2aafa156565c63bc05fdc5e4a1f05
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-17 16:58:55 +0900

    gnu: aspell: Update to 0.60.8.1.
    * gnu/packages/aspell.scm (aspell): Update to 0.60.8.1.
    [patches]: Remove aspell-CVE-2019-25051.patch.
    [phases]: Delete trailing #t.
    * gnu/packages/patches/aspell-CVE-2019-25051.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Unregister it.
    
    Change-Id: I58f8e51d084415f88ce7f0d5b31b23bbf4084018

--- gnu/packages/aspell.scm
+++ gnu/packages/aspell.scm
@@ -48,7 +48,7 @@
 (define-public aspell
   (package
     (name "aspell")
-    (version "0.60.8")
+    (version "0.60.8.1")
     (source
      (origin
       (method url-fetch)
@@ -56,9 +56,8 @@
                           version ".tar.gz"))
       (sha256
        (base32
-        "1wi60ankalmh8ds7nplz434jd7j94gdvbahdwsr539rlad8pxdzr"))
-      (patches (search-patches "aspell-default-dict-dir.patch"
-                               "aspell-CVE-2019-25051.patch"))))
+        "06yjhwlgpcvbrl5g83qjrpzjwjx7hka5lhsfc3x5gm229nri5nnn"))
+      (patches (search-patches "aspell-default-dict-dir.patch"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
@@ -74,8 +73,7 @@
                (substitute* "common/config.cpp"
                  (("\"filter-path(.*)DICT_DIR" _ middle)
                   (string-append "\"filter-path" middle
-                                 "\"" libdir "\"")))
-               #t))))))
+                                 "\"" libdir "\"")))))))))
     (native-inputs (list perl))
 
     (native-search-paths
