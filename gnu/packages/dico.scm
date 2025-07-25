wahjava/guix

commit a33e152f2eb9927be5942b00bdaaecba737c650f

gnu: dico: Update to 2.12.
* gnu/packages/dico.scm (dico): Update to 2.12.
[arguments]<#:phases>: Honor "tests?" flag in the custom "check" phase.
[home-page]: Set to "https://www.gnu.org.ua/software/dico/" as the original
page redirects here.

Change-Id: I021bc4740bb99fdd57bbe15317b8ee4951e6ba81

gnu/packages/dico.scm
@@ -4,6 +4,7 @@
 ;;; Copyright © 2018, 2020, 2021 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2020 Vincent Legoll <vincent.legoll@gmail.com>
 ;;; Copyright © 2022 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2025 Artyom V. Poptsov <poptsov.artyom@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -41,14 +42,14 @@
 (define-public dico
   (package
     (name "dico")
-    (version "2.11")
+    (version "2.12")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/dico/dico-"
                                   version ".tar.xz"))
               (sha256
                (base32
-                "0nic4mggc0yhms130k7x4qp5k9c42fwg6n8hmk5cmynh6gi9h7xc"))))
+                "1xvahrav8aml90qcj4cj3a33y0n7nm1k0ywgks1zy2q91v2qk2vj"))))
     (build-system gnu-build-system)
     (arguments
      '(#:configure-flags (list (string-append "--with-guile-site-dir=" %output
@@ -63,9 +64,10 @@
                       (setenv "GUILE_AUTO_COMPILE" "0")
                       (setenv "GUILE_WARN_DEPRECATED" "no")))
                   (replace 'check
-                    (lambda _
-                      ;; Test '71: append + dooffs + env' fails if $V is not 2.
-                      (invoke "make" "check" "V=2"))))))
+                    (lambda* (#:key tests? #:allow-other-keys)
+                      (when tests?
+                        ;; Test '71: append + dooffs + env' fails if $V is not 2.
+                        (invoke "make" "check" "V=2")))))))
     (native-inputs (list groff))
     (inputs
      (list m4                           ;used at run time
@@ -78,7 +80,7 @@
            wordnet
            libxcrypt                              ;for 'crypt'
            libltdl))
-    (home-page "https://www.gnu.org/software/dico/")
+    (home-page "https://www.gnu.org.ua/software/dico/")
     (synopsis "Implementation of DICT server (RFC 2229)")
     (description
      "GNU Dico implements a flexible dictionary server and client according to

commit 989b92eb42f1dbb9875744e65e882ae8c51e3bc9

gnu: dico: Hard-code file name of the default shell.
This fixes a regression introduced in
a33e152f2eb9927be5942b00bdaaecba737c650f whereby the ‘dicod’ Shepherd
service would fail to start with:

  dicod[2]: /gnu/store/…-m4-1.4.19/bin/m4 -s exited with status 127

… due to /bin/sh missing in its least-authority environment and ‘SHELL’
being unset.

* gnu/packages/dico.scm (dico)[arguments]: Add ‘set-shell-file-name’
phase.
[inputs]: Add ‘bash-minimal’.

Change-Id: Ie6f8a7a462a2dedcbf7ea049e385e121da06e741

gnu/packages/dico.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2015-2016, 2018, 2024 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2015-2016, 2018, 2024-2025 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2016, 2018 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2018, 2020, 2021 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2020 Vincent Legoll <vincent.legoll@gmail.com>
@@ -27,6 +27,7 @@
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages crypto)
   #:use-module (gnu packages readline)
   #:use-module (gnu packages m4)
@@ -56,6 +57,14 @@
                                               "/share/guile/site/2.0")
                                "--disable-static")
        #:phases (modify-phases %standard-phases
+                  (add-before 'build 'set-shell-file-name
+                    (lambda* (#:key inputs #:allow-other-keys)
+                      ;; This code invokes "/bin/sh -c 'm4 -s ...'".
+                      (substitute* "grecs/src/grecs-lex.c"
+                        (("\"/bin/sh\"")
+                         (string-append "\""
+                                        (search-input-file inputs "/bin/sh")
+                                        "\"")))))
                   (add-before 'check 'silence-guile
                     (lambda _
                       ;; Guile is too talkative, which disturbs the test
@@ -65,6 +74,7 @@
     (native-inputs (list groff))
     (inputs
      (list m4                           ;used at run time
+           bash-minimal                 ;likewise
            pcre
            python-wrapper
            guile-2.2