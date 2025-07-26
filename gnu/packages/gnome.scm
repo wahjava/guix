4a4687d0cc8090178347f13f6a63e6690bae2b9a
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-26 22:57:51 +0900

    gnu: gupnp: Update to 1.6.9.
    * gnu/packages/gnome.scm (gupnp): Update to 1.6.9.
    
    Change-Id: I3355fd749d13908be855eef05ab33a829868fd23

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -1428,7 +1428,7 @@ a debugging tool, @command{gssdp-device-sniffer}.")
 (define-public gupnp
   (package
     (name "gupnp")
-    (version "1.6.8")
+    (version "1.6.9")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -1436,7 +1436,7 @@ a debugging tool, @command{gssdp-device-sniffer}.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1s8i3vxg1hfxa7bcabwbfvw3n802z6gz8siyzf9p4mv8pp70783h"))))
+                "16w606l3v0q62v2y5qnl15z1n597xs53cdc7acpycn1mc7inxnrf"))))
     (build-system meson-build-system)
     (arguments
      (list
