6e232eb721975a900cfa39e7961c632449ffe6f0
Zheng Junjie <z572@z572.online>
2025-08-25 00:32:01 +0800

    gnu: krdp: Update to 6.4.3.
    * gnu/packages/kde-plasma.scm (krdp): Update to 6.4.3.
    [inputs]: Remove freerdp; add freerdp-3.
    
    Change-Id: If2e53aeb3e600c68737e4c4a0749ad10decf84f1

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -490,7 +490,7 @@ desktop.")
 (define-public krdp
   (package
     (name "krdp")
-    (version "6.3.6")
+    (version "6.4.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
@@ -498,7 +498,7 @@ desktop.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "15ajcclx3zb5rwd0yrfvns7dh5ijn9i6a5hfb2qjfyv1adzqx22v"))))
+                "11xphz2q201zwgfq9njh5vwqhc9acf0nwryw40slpw906qwm0jdh"))))
     (build-system qt-build-system)
     (arguments (list #:qtbase qtbase
                      #:tests? #f
@@ -528,7 +528,7 @@ desktop.")
              kpipewire
              openssl
              plasma-wayland-protocols
-             freerdp
+             freerdp-3
              qtwayland
              qtdeclarative
              qtkeychain-qt6
