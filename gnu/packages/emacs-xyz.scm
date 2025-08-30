22c33e7bb0892074442a514ffca80c84d59ad6d8
Cayetano Santos <csantosb@inventati.org>
2025-08-30 16:01:35 +0200

    gnu: emacs-pr-review: Update to 0.2.0.
    * gnu/packages/emacs-xyz.scm (emacs-pr-review): Update to 0.2.0.
    
    Change-Id: Ib13dc1f3f1de119c9a8b903f71614bdf74371b81

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -32198,7 +32198,7 @@ accept and reject GitHub pull requests.")
 (define-public emacs-pr-review
   (package
     (name "emacs-pr-review")
-    (version "0.1.0")
+    (version "0.2.0")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -32207,7 +32207,7 @@ accept and reject GitHub pull requests.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1cm92263jqvq2lg378xqi8ikbqw98lxjpsl29sja2xg2wf6p7gml"))))
+                "0ryf2jk54iqg7q494qdghg2pkhw8ky3s53dpj55871x6p2m1387r"))))
     (build-system emacs-build-system)
     (propagated-inputs (list emacs-magit emacs-ghub emacs-markdown-mode))
     (synopsis "Review GitHub Pull Requests")
