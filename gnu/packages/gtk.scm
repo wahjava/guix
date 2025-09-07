31f7919a9733f809c63d10da50c18cc2a0500fdd
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-09-07 10:05:17 +0200

    gnu: gtk: Use tinysparql.
    * gnu/packages/gtk.scm (gtk): Replace tracker with tinysparql.

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -1385,7 +1385,7 @@ application suites.")
            libtiff
            python
            rest
-           tracker))                    ;for filechooser search support
+           tinysparql))                 ;for filechooser search support
     (propagated-inputs
      ;; Following dependencies are referenced in .pc files.
      (list cairo
