a8753c793e59fdffce4c8432bcc0c27487e0bf81
Rutherther <rutherther@ditigal.xyz>
2025-09-07 14:11:47 +0200

    gnu: make-nsis: Support cross compilation.
    Here a hack has been added as there are two cross compilation toolchains
    in the build process when cross compiling. The cross compile toolchain used
    when using --target has been wrapped with the proper environment. The mingw
    toolchain is using environment variables the same way it does so for native build.
    
    * gnu/packages/installers.scm ((make-nsis machine target-arch nsis-target-type)): Update to 3.11.
    [native-inputs]: Add bash-minimal.
    
    Change-Id: Ieb64d5f5acbcf3fc9e17df5519344dda00cad53e

--- gnu/packages/installers.scm
+++ gnu/packages/installers.scm
@@ -23,6 +23,8 @@
   #:use-module (gnu packages build-tools)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages cross-base)
+  #:use-module (gnu packages bash)
+  #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system scons)
@@ -47,68 +49,116 @@
       (build-system scons-build-system)
       (native-inputs `(("xgcc" ,xgcc)
                        ("xbinutils" ,xbinutils)
-                       ("mingw-w64" ,xlibc)))
+                       ("mingw-w64" ,xlibc)
+                       ("bash" ,bash-minimal)))
       (inputs (list zlib))
       (arguments
-       `(#:scons ,scons-3
-         #:modules ((srfi srfi-1)
+       (list
+        #:scons scons-3
+        #:modules '((srfi srfi-1)
                     (srfi srfi-26)
                     (guix build utils)
                     (guix build scons-build-system))
-         #:tests? #f
-         #:scons-flags `("UNICODE=yes"
+        #:tests? #f
+        #:scons-flags #~(cons*
+                         "UNICODE=yes"
                          "SKIPUTILS=MakeLangId,Makensisw,NSIS Menu,SubStart,zip2exe"
                          "SKIPDOC=COPYING"
                          "STRIP_CP=no"
-                         ,(string-append "PREFIX=" %output)
-                         ,(string-append "TARGET_ARCH=" ,target-arch)
-                         ,(string-append "XGCC_W32_PREFIX=" ,triplet "-")
-                         ,(string-append "PREFIX_PLUGINAPI_INC=" (assoc-ref %build-inputs "mingw-w64") "/include/")
-                         ,(string-append "PREFIX_PLUGINAPI_LIB=" (assoc-ref %build-inputs "mingw-w64") "/lib/"))
-         #:build-targets '("makensis"
+                         (string-append "PREFIX=" #$output)
+                         (string-append "TARGET_ARCH=" #$target-arch)
+                         (string-append "XGCC_W32_PREFIX=" #$triplet "-")
+                         (string-append "PREFIX_PLUGINAPI_INC=" #+(this-package-native-input "mingw-w64") "/include/")
+                         (string-append "PREFIX_PLUGINAPI_LIB=" #+(this-package-native-input "mingw-w64") "/lib/")
+                         (if #$(%current-target-system)
+                             (list
+                              (string-append "CC=" #$(cc-for-target) "-wrapper")
+                              (string-append "CXX=" #$(cxx-for-target) "-wrapper"))
+                             '()))
+        #:build-targets ''("makensis"
                            "stubs"
                            "plugins"
                            "utils")
-         #:install-targets '("install-stubs"
+        #:install-targets ''("install-stubs"
                              "install-plugins"
                              "install-data"
                              "install-utils"
                              "install-compiler"
                              "install-conf")
-         #:phases (modify-phases %standard-phases
-                    (add-before 'build 'fix-env
-                      (lambda _
-                        (define* (filter-delimited-string delimited-string predicate #:optional (delimiter #\:))
-                          ;; Given a DELIMITED-STRING delimited by DELIMITER,
-                          ;; only keep items that satisfy PREDICATE
-                          (string-join
-                           (filter predicate (string-split delimited-string delimiter))
-                           (string delimiter)))
-                        (define (mingw-path? path)
-                          (string-prefix? (assoc-ref %build-inputs "mingw-w64") path))
-                        (define (xgcc-path? path)
-                          (string-prefix? (assoc-ref %build-inputs "xgcc") path))
-                        (define (cross-toolchain-path? path)
-                          (or (xgcc-path? path) (mingw-path? path)))
+        #:phases
+        #~(modify-phases %standard-phases
+            (add-before 'build 'fix-env
+              (lambda* (#:key target #:allow-other-keys)
+                (define* (filter-delimited-string delimited-string predicate #:optional (delimiter #\:))
+                  ;; Given a DELIMITED-STRING delimited by DELIMITER,
+                  ;; only keep items that satisfy PREDICATE
+                  (string-join
+                   (filter predicate (string-split delimited-string delimiter))
+                   (string delimiter)))
+                (define (mingw-path? path)
+                  (string-prefix? (assoc-ref %build-inputs "mingw-w64") path))
+                (define (xgcc-path? path)
+                  (string-prefix? (assoc-ref %build-inputs "xgcc") path))
+                (define (cross-toolchain-path? path)
+                  (or (xgcc-path? path) (mingw-path? path)))
 
-                        (for-each
-                         (lambda (env-name)
-                           (let ((env-val (getenv env-name)))
-                             ;; Remove all mingw-w64 and xgcc paths from env vars meant
-                             ;; for native toolchain
-                             (setenv env-name
-                                     (filter-delimited-string env-val (negate cross-toolchain-path?)))
-                             ;; Add the removed paths back into CROSS_-prefixed
-                             ;; version of env vars
-                             (setenv (string-append "CROSS_" env-name)
-                                     (filter-delimited-string env-val cross-toolchain-path?))))
-                         '("C_INCLUDE_PATH" "CPLUS_INCLUDE_PATH" "LIBRARY_PATH"))))
-                    (add-before 'build 'fix-target-detection
-                      (lambda _
-                        ;; NSIS target detection is screwed up, manually change
-                        ;; it ourselves
-                        (substitute* "Source/build.cpp" (("m_target_type=TARGET_X86UNICODE")
-                                                         (string-append "m_target_type=" ,nsis-target-type))))))))
+                (for-each
+                 (lambda (env-name)
+                   (let ((env-val (getenv env-name)))
+                     ;; Remove all mingw-w64 and xgcc paths from env vars meant
+                     ;; for native toolchain
+                     (setenv env-name
+                             (filter-delimited-string env-val (negate cross-toolchain-path?)))
+                     ;; Add the removed paths back into CROSS_-prefixed
+                     ;; version of env vars
+                     (setenv (string-append "CROSS_" env-name)
+                             (filter-delimited-string env-val cross-toolchain-path?))))
+                 '("C_INCLUDE_PATH" "CPLUS_INCLUDE_PATH" "LIBRARY_PATH"))))
+            (add-before 'fix-env 'fix-cross-compilation
+              (lambda* (#:key target #:allow-other-keys)
+                (when target
+                  (mkdir-p "./cc-wrapper")
+                  (setenv "PATH"
+                          (string-append
+                           (getcwd) "/cc-wrapper"
+                           ":"
+                           (getenv "PATH")))
+
+                  ;; Make a wrapper for the CC and CXX, setting proper
+                  ;; CROSS_* env vars
+                  (call-with-output-file "./cc-wrapper/env"
+                    (lambda (port)
+                      (format port "#!~a/bin/sh~%" #$(this-package-native-input "bash"))
+                      (for-each
+                       (lambda (env-var)
+                         (format port
+                                 "export ~a=\"~a\"~%"
+                                 env-var
+                                 (getenv env-var))
+                         (unsetenv env-var))
+                         '("CROSS_C_INCLUDE_PATH" "CROSS_CPLUS_INCLUDE_PATH" "CROSS_LIBRARY_PATH"))))
+
+                  (for-each
+                   (lambda (file)
+                     (let ((target-file
+                            (string-append "./cc-wrapper/" #$(%current-target-system) "-" file "-wrapper")))
+                       (call-with-output-file target-file
+                         (lambda (port)
+                           (format port "#!~a/bin/sh~%" #$(this-package-native-input "bash"))
+                           (format port
+                                   "source ~a/cc-wrapper/env~%exec ~a-~a \"$@\""
+                                   (getcwd) #$(%current-target-system) file)))
+                       (chmod target-file #o777)))
+                   '("gcc" "g++")))))
+            ;; (add-after 'fix-env 'xyz
+            ;;   (lambda _
+            ;;     (exit 1)))
+            (add-before 'build 'fix-target-detection
+              (lambda _
+                ;; NSIS target detection is screwed up, manually change
+                ;; it ourselves
+                (substitute* "Source/build.cpp" (("m_target_type=TARGET_X86UNICODE")
+                                                 (string-append "m_target_type=" #$nsis-target-type))))))))
       (home-page "https://nsis.sourceforge.io/Main_Page")
       (synopsis "System to create Windows installers")
       (description
