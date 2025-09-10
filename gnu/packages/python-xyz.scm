cc5b500ede82e09e48f84148d20627504a6a3a37
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2025-09-10 13:10:22 +0200

    gnu: python-glob2: Switch to pyproject-build-system.
    * gnu/packages/python-xyz.scm (python-glob2): Switch to pyproject-build-system.
    [build-sytem]: Switch to pyproject-build-system.
    [native-inputs]: Add python-setuptools, python-wheel, and python-pytest.
    [arguments] <#:phases>: Add fixes for pytest 8.
    
    Change-Id: I95459df0aedf4c5ba22d060aa1e2640875ecb240

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -29216,7 +29216,18 @@ compatible with @code{asyncio}.")
        (file-name (git-file-name name version))
        (sha256
         (base32 "160nh2ay9lw2hi0rixpzb2k87r6ql56k0j2cm87lqz8xc8zbw919"))))
-    (build-system python-build-system)
+    (build-system pyproject-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'patch-tests-for-pytest8
+           ;; See: <https://github.com/miracle2k/python-glob2/issues/30>.
+           (lambda _
+             (substitute* "test.py"
+               (("setup\\(") "setup_method(")
+               (("teardown\\(") "teardown_method("))
+             (rename-file "test.py" "glob2_test.py"))))))
+    (native-inputs (list python-setuptools python-wheel python-pytest))
     (home-page "https://github.com/miracle2k/python-glob2/")
     (synopsis "Extended Version of the python buildin glob module")
     (description "This is an extended version of the Python
