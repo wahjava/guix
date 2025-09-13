8be9dc15d05f66dc2edadc70f13369969717356b
Nicolas Graves <ngraves@ngraves.fr>
2025-09-13 16:59:39 +0200

    gnu: python-tokenizers: Extend with (pyproject-guile-json).
    * gnu/packages/machine-learning.scm (python-tokenizers)[arguments]
    <#:phases>: Extend with (pyproject-guile-json).

--- gnu/packages/machine-learning.scm
+++ gnu/packages/machine-learning.scm
@@ -5299,6 +5299,7 @@ definite approximations of Optimal Transport (Wasserstein) distances.
                   ((guix build pyproject-build-system) #:prefix py:)
                   (guix build utils))
       #:phases
+      (with-extensions (list (pyproject-guile-json))
       #~(modify-phases %standard-phases
           (add-after 'check 'python-check
             (lambda _
@@ -5322,7 +5323,7 @@ definite approximations of Optimal Transport (Wasserstein) distances.
                 (copy-file "PKG-INFO" (string-append info "/METADATA"))
                 (copy-recursively
                  "py_src/tokenizers"
-                 (string-append lib "tokenizers"))))))))
+                 (string-append lib "tokenizers")))))))))
     (native-inputs
      (list pkg-config python-minimal python-pytest))
     (inputs
