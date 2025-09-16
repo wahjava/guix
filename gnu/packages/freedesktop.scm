ddc1b0dba7df1e422ca6c6362605b9c805cf9c61
Zheng Junjie <z572@z572.online>
2025-09-17 00:01:29 +0800

    gnu: libinput: Update to 1.29.1.
    * gnu/packages/freedesktop.scm (libinput): Update to 1.29.1.
    
    Change-Id: I36585c099d7b58fd335a370ab03509c559d35def

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -679,7 +679,7 @@ freedesktop.org project.")
   ;; Updating this will rebuild over 700 packages through libinput-minimal.
   (package
     (name "libinput")
-    (version "1.26.2")
+    (version "1.29.1")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -688,7 +688,7 @@ freedesktop.org project.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1zwwq7a0a6yznc6jxhp6gb50yw5vpfkvgbrabrpc5pwldpckfbrg"))))
+                "11n2vqkdz40vbqdjwm19i7rv2lzqf4i7anlla0havf7h0glqin60"))))
     (build-system meson-build-system)
     (arguments
      `(#:configure-flags '("-Ddocumentation=false")
