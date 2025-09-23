4a5ead4753a601b4eb61b3ec29ea08030616d4df
ColdSideOfYourPillow <ColdSideOfYourPillow@noreply.codeberg.org>
2025-09-23 19:05:47 +0400

    gnu: Add mpv-thumbfast.
    * gnu/packages/video.scm (mpv-thumbfast): New variable.
    
    Change-Id: I58ad120c67e3a4199db63bc1e2a7d636a3e07418

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -2769,6 +2769,35 @@ fork of mplayer2 and MPlayer.  It shares some features with the former
 projects while introducing many more.")
     (license license:gpl2+)))
 
+(define-public mpv-thumbfast
+  ;; Latest commit as of 2025-09-23
+  (let ((commit "9deb0733c4e36938cf90e42ddfb7a19a8b2f4641")
+        (revision "0"))
+    (package
+      (name "mpv-thumbfast")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+                (url "https://github.com/po5/thumbfast")
+                (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0q537fjj9ndq7pzg2rv4h5qas8s3812k21bpw064bcvb204vbwba"))))
+      (build-system copy-build-system)
+      (arguments
+       (list
+        #:install-plan
+        #~'(("thumbfast.lua" "share/mpv/scripts/"))))
+      (home-page "https://github.com/po5/thumbfast")
+      (synopsis "High-performance on-the-fly thumbnailer script for mpv")
+      (description
+       "Thumbfast is a high-performance on-the-fly thumbnailer script for
+mpv. @{This script does not display thumbnails on its own}, it is meant to be
+used alongside a @{UI, User Interface} script that calls it.")
+      (license license:mpl2.0))))
+
 (define-public smplayer
   (package
     (name "smplayer")
